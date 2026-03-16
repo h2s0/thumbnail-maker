a1ca841a1b6775ebdc6abac8e6311b7e550558ca
heesae <134567479+h2s0@users.noreply.github.com>
2026-03-16 12:35:20 +0900

    (또) 폰트크기 수정

--- script.js
+++ script.js
@@ -136,7 +136,7 @@ function draw() {
   const text = textInput.value;
   if (text) {
     const shortSide = Math.min(canvas.width, canvas.height)
-    const fontSize = Math.floor(shortSide * 0.15)
+    const fontSize = Math.floor(shortSide * 0.145)
     const lineHeight = fontSize
 
     ctx.font = `bold ${fontSize}px ${currentFont}`;
